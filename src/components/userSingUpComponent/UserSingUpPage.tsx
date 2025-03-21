a2fc986f773284ab94f116ef3685ab7b9c05c885
Vitaliy <vitaly.matyko@mail.ru>
2025-03-21 15:44:24 +0300

    Fixed register of name files.

--- src/components/userSingUpComponent/UserSingUpPage.tsx
+++ src/components/userSingUpComponent/UserSingUpPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UserSingUpPageProps } from "../../types/types";
-import CreateAccountForm from "./createAccountForm";
+import CreateAccountForm from "./CreateAccountForm"
 
 
 
